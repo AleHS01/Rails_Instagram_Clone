e9286dbedaded5fee07591c395e1e5693cf4480a
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-12 23:59:55 -0400

    form bootsrapify, and better error handling, also we modify the model so that it doesnt accept blank input  and min-max length included

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 5, maximun: 100}
+    validates :description, presence:true, length: {minimum: 5, maximun: 500}
+    validates :keywords, presence:true, length: {minimum: 5, maximun: 100}
 end
