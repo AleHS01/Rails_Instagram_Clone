dd4209f17cc6f1f76dbda3f693a71184792675c3
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-13 23:50:04 -0400

    adding support for image part 1, we add active storage to allow images/videos support as a model, and we edit the controler and model of post to allow images as an array to be attacthed when form submissions and so on

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5, maximun: 100}
     validates :description, presence:true, length: {minimum: 5, maximun: 500}
     validates :keywords, presence:true, length: {minimum: 5, maximun: 100}
+
+    has_many_attached :images
 end
