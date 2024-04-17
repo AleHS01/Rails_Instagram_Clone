64780ec93e139504f3f4f953a8f57afc2d822dcf
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-16 23:43:35 -0400

    style login, signup, edit account form to have consistent look and more appealing zsh: command not found: style

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
     get "/users/password", to: "devise/passwords#new"
     get "/users/sign_out" => "devise/sessions#destroy"
   end
+  devise_for :users
 
 
   get 'home/about'
-  devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
