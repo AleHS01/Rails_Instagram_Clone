08806d3b85e9c269e0711da40af871e99dfc5f7a
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-04-17 23:52:04 -0400

    added a  new field to post: user_id using - rails g migration - that will be the common field between user and post table, also added a before_create to randomize the id, and added a before_action to make sure user is authenticated

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,13 @@ class Post < ApplicationRecord
     validates :keywords, presence:true, length: {minimum: 5, maximun: 100}
 
     has_many_attached :images
+    belongs_to :user
+
+    before_create :randomize_id
+         private
+         def randomize_id
+            begin
+              self.id = SecureRandom.random_number(1_000_000_000)
+            end while Post.where(id: self.id).exists?
+         end
 end
