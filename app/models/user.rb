7485b8be9f22e205db971e413cb1af2a5ccf013d
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2024-05-02 00:59:47 -0400

    addind follow/unfollow accept and decline follow request functionality using a gem called followability

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+        followability
+
         has_many :post
         has_many :comments
         has_one_attached :avatar
         has_many :likes
-         before_create :randomize_id
+        before_create :randomize_id
+
+        def unfollow(user)
+          followerable_relationships.where(followed_id: user.id).destroy_all
+        end
+
          private
          def randomize_id
             begin
